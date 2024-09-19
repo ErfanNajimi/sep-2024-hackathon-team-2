d52f7b729a4b6a171fea3210f970cfda97cad620
Noah Samawi <noah.alsamawi@gmail.com>
2024-09-19 12:34:04 +0000

    Update enp file

---
+++ enp.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://ukvq42rgibe:IylY03LplSsX@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/rake_wolf_wagon_216484"
+os.environ["SECRET_KEY"] = "XVff0ksfsCK^:nHis[;F''yW)n_OZghd}~J27zB7=ift#rKA6Xgh9s:oO1t0[kNnYJ1B,wz%nh"
+os.environ["DEBUG"] = "1"
