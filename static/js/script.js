95306b4419072d4e8ab9b389fa1aec9b92b8840a
Markpm-code <marlonseglos@yahoo.com>
2024-09-21 10:27:05 +0000

    add keydown listener

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,26 @@
 /* Piano */
 
-const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B' ];
+const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+const keyBindings = ['A', 'W', 'S', 'E', 'D', 'F', 'T', 'G', 'Y', 'H', 'U', 'J']; 
 
-for (index in keys) {
-	let element = document.getElementById(`${keys[index]}`);
-	element.addEventListener('click', e => {
-	console.log(`${element.id}`);
-    document.getElementById(`${element.id}-audio`).load();
-    document.getElementById(`${element.id}-audio`).play();
-	})
+for (let index in keys) {
+    let element = document.getElementById(`${keys[index]}`);
+    element.addEventListener('click', e => {
+        console.log(`${element.id}`);
+        document.getElementById(`${element.id}-audio`).load();
+        document.getElementById(`${element.id}-audio`).play();
+    });
 }
+
+document.addEventListener('keydown', e => {
+    const keyIndex = keyBindings.indexOf(e.key.toUpperCase());
+    if (keyIndex !== -1) {
+        let note = keys[keyIndex];
+        console.log(note);
+        document.getElementById(`${note}-audio`).load();
+        document.getElementById(`${note}-audio`).play();
+    }
+});
 
 /* Gameplay stages */
 
