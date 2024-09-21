3b9d9e559dae1243c403fb04200b1b4fd9d43051
Erfan Najimi <erfannajimi123@gmail.com>
2024-09-21 16:08:36 +0000

    Fix bug in models due to typo

--- piano/models.py
+++ piano/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Tune(models.Model):
@@ -13,7 +14,7 @@ class Tune(models.Model):
         return f"{self.name}"
 
 class Score(models.Model):
-    player = models.ForeignKey(User, on_delete=Cmodels.CASCADE, name = "player")
+    player = models.ForeignKey(User, on_delete=models.CASCADE, name = "player")
     score = models.IntegerField(default=0)
 
     class Meta: 
