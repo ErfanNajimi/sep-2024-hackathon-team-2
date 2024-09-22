e9dda170f266fb0960a0e0d2000a733f9249308c
Noah Samawi <noah.alsamawi@gmail.com>
2024-09-22 19:25:42 +0000

    Add authentication and email settings to settings.py ,Update piano URLs and views to improve functionality , Fix profile.html template to correctly display user profile images , Add profile link to navbar for user access

--- sep_2024_hackathon/urls.py
+++ sep_2024_hackathon/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('piano.urls'), name='piano.urls'),
+    path('accounts/', include('allauth.urls')),  
+    path('admin/', admin.site.urls),              
+    path('', include('piano.urls')),          
 ]
+
